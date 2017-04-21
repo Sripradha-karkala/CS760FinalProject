86e6c7b20ea88c3153b2ed7fa86931362c00a3ed
Jamie Warner <jamiesonwarner@gmail.com>
2017-04-21 01:55:47 -0500

    add genetic search. run it with python ga.py smallData.csv.

--- ca.py
+++ ca.py
@@ -11,68 +11,85 @@ b = np.array([2.,4.,6.])
 data = np.array([a,b])
 
 class Data:
-	def __init__(self, file_name, testset_split):
-		with open(file_name) as input_file:
-			csv.reader(input_file, delimiter = ',')
-			data = list(input_file)
-		data = data[12:] # first 12 lines have comments
-		data = [numbers.split(',') for numbers in data] 
-		d = np.array(data)[:,1:].astype(int) # first column has date
-		trainset_size = int(math.ceil((1.0 - testset_split) * len(d)))
-		self.train_data = d[:trainset_size,:]
-		self.test_data = d[trainset_size:,:] 
+    def __init__(self, file_name, testset_split):
+        with open(file_name) as input_file:
+            csv.reader(input_file, delimiter = ',')
+            data = list(input_file)
+        data = data[12:] # first 12 lines have comments
+        data = [numbers.split(',') for numbers in data]
+        d = np.array(data)[:,1:].astype(int) # first column has date
+        trainset_size = int(math.ceil((1.0 - testset_split) * len(d)))
+        self.train_data = d[:trainset_size,:]
+        self.test_data = d[trainset_size:,:]
 
 class UpdateRule:
-	def __init__(self, neighborhood_size):
-		self.weights = np.random.rand(neighborhood_size+2) # +1 for self, +1 for bias
-	
-	def __call__(self, cell_value, neighbor_values):
-		return self.weights[0] * cell_value \
-			+ self.weights[1:-1].dot(neighbor_values) + self.weights[-1]
+    def __init__(self, neighborhood_size, weights = None):
+        self.neighborhood_size = neighborhood_size
+        if weights is None:
+            self.weights = self.make_weights(1)
+        else:
+            self.weights = weights
 
-	
+    def __call__(self, cell_value, neighbor_values):
+        return self.weights[0] * cell_value \
+            + self.weights[1:-1].dot(neighbor_values) + self.weights[-1]
+
+    def make_weights(self, magnitude):
+        return magnitude * np.random.rand(self.neighborhood_size + 2) # +1 for self, +1 for bias
+
+    def perturb(self, amount):
+        offset = self.make_weights(amount)
+        return UpdateRule(self.neighborhood_size, self.weights + offset)
+
+
 class CellularAutomaton:
-	def __init__(self, initial_values, update_rule):
-		self.values = initial_values
-		self.update_rule = update_rule
+    def __init__(self, initial_values, update_rule):
+        self.values = np.copy(initial_values)
+        self.update_rule = update_rule
 
-	def get_neighbors(self, i):
-		return np.append(self.values[:i], self.values[i+1:])
+    def get_neighbors(self, i):
+        return np.append(self.values[:i], self.values[i+1:])
 
-	def update(self):
-		for i in range(len(self.values)):
-			neighbors = self.get_neighbors(i)
-			self.values[i] = self.update_rule(self.values[i], neighbors)
+    def update(self):
+        for i in range(len(self.values)):
+            neighbors = self.get_neighbors(i)
+            self.values[i] = self.update_rule(self.values[i], neighbors)
 
-	def get_values(self):
-		return self.values
+    def get_values(self):
+        return self.values
 
-class Evaluator:
-	def __init__(self, data, batch_size, neighborhood_size):
-		update_rule = UpdateRule(neighborhood_size)
-		ca = CellularAutomaton(data[0], update_rule)
-		error = 0.0
-		for t in range(batch_size):
-			ca.update()
-			error += sum(abs(ca.get_values() - data[t]))
-			print ca.get_values()
+def evaluate_rule(rule, data, batch_size):
+    ca = CellularAutomaton(data[0], rule)
+    error = 0.0
+    debug_errors = []
+    for t in range(batch_size):
+        ca.update()
+        error += sum(abs(ca.get_values() - data[t]))
+        debug_errors.append(error)
+    # print 'cumulative errors: %s' % debug_errors
+    return error
 
 def main(args):
-	data = Data(args.input_file, args.split)
-	Evaluator(data.train_data, args.batch, args.neighbor)
+    data = Data(args.input_file, args.split)
+    rule = UpdateRule(args.neighbor)
+    evaluate_rule(rule, data.train_data, args.batch)
+
+def make_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', type = str,
+            help='Specify the path/filename of the input data.')
+    parser.add_argument('-s', '--split', type = float, default = 0.3,
+            help = 'Specify the portion of data to use as testset, e.g. 0.3.')
+    parser.add_argument('-n', '--neighbor', type = int, default = 2,
+            help = 'Specify the number of neighbors to use.')
+    parser.add_argument('-b', '--batch', type = int, default = 2,
+            help = 'Specify the size of the data window in a batch.')
+    return parser
 
 def parse_args():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('input_file', type = str,  
-			help='Specify the path/filename of the input data.')
-	parser.add_argument('-s', '--split', type = float, default = 0.3,  
-			help = 'Specify the portion of data to use as testset, e.g. 0.3.')
-	parser.add_argument('-n', '--neighbor', type = int, default = 2,
-			help = 'Specify the number of neighbors to use.')
-	parser.add_argument('-b', '--batch', type = int, default = 2,
-			help = 'Specify the number of batches to run.')
-	return parser.parse_args()
+    parser = make_argument_parser()
+    return parser.parse_args()
 
 if __name__ == '__main__':
-	main(parse_args())
+    main(parse_args())
 
---
+++ ga.py
@@ -0,0 +1,77 @@
+from ca import make_argument_parser, Data, UpdateRule, CellularAutomaton, evaluate_rule
+import random
+import numpy as np
+
+POPULATION_SIZE = 100
+N_GENERATIONS = 100
+PERTURB_AMOUNT = 0.1 # I dunno
+SURVIVOR_RATIO = 0.2 # on average 20% of the population survives
+
+# Comment or uncommont to change the pruning strategy
+USE_SELECTION_STRATEGY_A = True
+# USE_SELECTION_STRATEGY_A = False
+
+def make_random_rule(neighborhood_size):
+    return UpdateRule(neighborhood_size)
+
+def genetic_train(args):
+    batch_size = args.batch
+    neighborhood_size = args.neighbor
+    data = Data(args.input_file, args.split)
+
+    # Initialize search at cells with random parameters
+    population = [make_random_rule(neighborhood_size) for _ in range(POPULATION_SIZE)]
+
+    # Iterate through generations:
+    for k in range(N_GENERATIONS):
+        print '== begin generation %s ==' % k
+        # Evaluate every CA in the population
+        evaluations = []
+        for update_rule in population:
+            try:
+                evaluations.append(evaluate_rule(update_rule, data.train_data, batch_size))
+            except OverflowError:
+                # Just append a really big weight if the CA is broken
+                evaluations.append(10000)
+
+        # Calculate the best and average error over this generation
+
+        average = float(sum(evaluations)) / len(evaluations)
+        best_index = 0
+        for i in range(len(population)):
+            if evaluations[i] <= evaluations[best_index]:
+                best_index = i
+        if USE_SELECTION_STRATEGY_A:
+            new_population = [population[best_index]]
+        print 'errors: best %s, average %s' % (evaluations[best_index], average)
+
+        # Prune population according to probability of survival
+        # TODO is inverse error a good fitness?
+        if not USE_SELECTION_STRATEGY_A:
+            fitnesses = [1/float(error) if error != 0 else 10000 for error in evaluations]
+            sum_fitness = sum(fitnesses)
+            new_population = []
+            for update_rule, fitness in zip(population, fitnesses):
+                # Probability of success is fraction of fitness of total
+                if float(fitness) / sum_fitness > random.random() * SURVIVOR_RATIO:
+                    new_population.append(update_rule)
+
+        if len(new_population) == 0:
+            print 'ah geez, no one lived ):'
+            new_population.append(population[0])
+
+        # Create next generation by perturbing the best performers.
+        generation_size = len(new_population)
+        for i in range(POPULATION_SIZE - generation_size):
+            # Cycle through survivors to be parents
+            parent = new_population[i % generation_size]
+            new_population.append(parent.perturb(PERTURB_AMOUNT)) # TODO consider making this smaller throughout training
+
+        population = new_population
+
+def parse_args():
+    parser = make_argument_parser()
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    genetic_train(parse_args())
